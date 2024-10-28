ebbb783c75506ebdecbec83f1e11234d30d7a658
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-28 19:13:48 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,11 @@ const app =express();
 dotenv.config({path:'./config/config.env'})
 
 app.use(cors({
-    origin:'https://khana-khazana-iota.vercel.app',
+    origin:'*',
     methods:["POST"],
     credentials:true
 }))
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://khana-khazana-iota.vercel.app'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 app.get("/",(req,res)=>{
     res.send("Hello");
 })
